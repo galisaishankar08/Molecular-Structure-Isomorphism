58993948b08e4254efbc9bcc57123097559a6ac6
SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2022-06-07 14:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def index():
 def output():
     return render_template('result.html')
 
+@app.route('/', methods=['POST'])
+def isomorphic():
+    if request.method == "POST":
+        mol1 = request.form["cname1"]
+        mol2 = request.form["cname2"]
+        res = mol1
+        return render_template("result.html", result=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
