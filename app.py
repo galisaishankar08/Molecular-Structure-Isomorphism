a92605ab0c0feda2006cc7c639cf43adda8f99ba
SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2022-06-07 14:43:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,7 @@ def isomorphic():
         mol1 = request.form["cname1"]
         mol2 = request.form["cname2"]
         
-        def CIR_convert(ids):
-            try:
-                url = 'http://cactus.nci.nih.gov/chemical/structure/' + quote(ids) + '/smiles'
-                ans = urlopen(url).read().decode('utf8')
-                return ans
-            except:
-                return 'Did not work'
-            
-        s_formula1 = CIR_convert(mol1)
-        
-        res = s_formula1
+        res = mol1
         return render_template("result.html", result=res)
 
 if __name__ == '__main__':
