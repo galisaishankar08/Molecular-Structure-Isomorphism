479b6d2dc43ab4b1ab5ef81abbda3b7e035136cb
SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2022-06-09 11:24:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import *
+import numpy as np
+import networkx as nx
+from networkx.algorithms import isomorphism
 
 
 app = Flask(__name__)
@@ -19,7 +22,22 @@ def isomorphic():
         mol1 = request.form["cname1"]
         mol2 = request.form["cname2"]
         
-        res = mol1
+        adj_matrix_c1 = np.array([[0, 1, 0, 0, 0],
+                          [1, 0, 1, 0, 1],
+                          [0, 1, 0, 1, 1],
+                          [0, 0, 1, 0, 1],
+                          [0, 1, 1, 1, 0]])
+
+        adj_matrix_c2 = np.array([[0, 1, 1, 1, 0],
+                          [1, 0, 1, 0, 0],
+                          [1, 1, 0, 1, 0],
+                          [1, 0, 1, 0, 1],
+                          [0, 0, 0, 1, 0]])
+        G1 = nx.from_numpy_matrix(adj_matrix_c1)
+        G2 = nx.from_numpy_matrix(adj_matrix_c2)
+        GM = isomorphism.GraphMatcher(G1, G2)
+        
+        res = GM.is_isomorphic()
         return render_template("result.html", result=res)
 
 if __name__ == '__main__':
